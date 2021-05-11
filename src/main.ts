12853584290fdba185da27de260b7e26676036d5
Arcuman <anton.borisov.17@mail.ru>
2021-05-11 12:56:31 +0300

    add try catch

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,31 @@ import sequelize from 'sequelize';
 import { Role } from './roles/roles.model';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  const config = new DocumentBuilder()
-    .setTitle('Optical Salon')
-    .setDescription('Documentation REST API')
-    .setVersion('1.0.0')
-    .addSecurity('bearer', {
-      type: 'http',
-      scheme: 'bearer',
-    })
-    .addServer('https://afternoon-waters-64991.herokuapp.com/api/')
-    .addServer('http://localhost:5000/api/')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api/docs', app, document);
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    const config = new DocumentBuilder()
+      .setTitle('Optical Salon')
+      .setDescription('Documentation REST API')
+      .setVersion('1.0.0')
+      .addSecurity('bearer', {
+        type: 'http',
+        scheme: 'bearer',
+      })
+      .addServer('https://afternoon-waters-64991.herokuapp.com/api/')
+      .addServer('http://localhost:5000/api/')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api');
-  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
+    app.useGlobalPipes(new ValidationPipe());
+    app.setGlobalPrefix('api');
+    await app.listen(PORT, () =>
+      console.log(`Server started on port = ${PORT}`),
+    );
+  } catch (e) {
+    console.log(e);
+  }
 }
 bootstrap();
